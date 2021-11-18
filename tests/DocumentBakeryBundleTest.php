8456f38b08b9aa1c68105f83b8b51d4075c5172b
AlexMenschoid <meyer-schlichtmann@demos-deutschland.de>
2021-11-18 17:12:19 +0100

    feat: adds basic bundle and extension tests
    but the bundle test is not yet working.

---
+++ tests/DocumentBakeryBundleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DemosEurope\DocumentBakery\Tests;
+
+use DemosEurope\DocumentBakery\Bakery;
+use DemosEurope\DocumentBakery\DocumentBakeryBundle;
+use Nyholm\BundleTest\BaseBundleTestCase;
+
+class DocumentBakeryBundleTest extends BaseBundleTestCase
+{
+    public function testBundle(): void
+    {
+        $kernel = $this->createKernel();
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+
+        $bundle = $container->get(Bakery::class );
+        $this->assertInstanceOf(Bakery::class, $bundle);
+    }
+
+    protected function getBundleClass(): string
+    {
+        return DocumentBakeryBundle::class;
+    }
+}
