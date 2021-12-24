c1c20e3947ed54ef24ac196640cef4a4cfa56b61
AlexMenschoid <meyer-schlichtmann@demos-deutschland.de>
2021-12-24 11:58:47 +0100

    minor: adds cell instruction test

---
+++ tests/Instructions/CellTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DemosEurope\DocumentBakery\Tests\Instructions;
+
+use DemosEurope\DocumentBakery\Instructions\Cell;
+use PhpOffice\PhpWord\Element\AbstractContainer;
+use PhpOffice\PhpWord\Element\Row;
+use PhpOffice\PhpWord\Element\Table;
+
+class CellTest extends InstructionsTestCase
+{
+    protected function setUp(): void
+    {
+        $this->setInstructionUnderTest(Cell::class);
+        $this->prepareData();
+    }
+
+    protected function prepareData(): void
+    {
+        // Instruction data is only used if the instruction has a path attribute and not content.
+        $this->recipeDataBag = $this->getDefaultRecipeDataBag('Testing Text');
+        $this->recipeDataBag->addToWorkingPath(new Table());
+        $this->recipeDataBag->addToWorkingPath(new Row());
+        $mappedStyles = $this->getDefaultMappedStyles();
+
+        $this->prepareInstruction([], $this->recipeDataBag, $mappedStyles);
+    }
+
+    public function testIsWorkingInstance(): void
+    {
+        self::assertInstanceOf(Cell::class, $this->instructionUnderTest);
+        $this->instructionUnderTest->render();
+        /**
+         * @var AbstractContainer $currentParentElement
+         */
+        $currentParentElement = $this->recipeDataBag->getCurrentParentElement();
+        self::assertInstanceOf(\PhpOffice\PhpWord\Element\Cell::class, $currentParentElement);
+    }
+}
