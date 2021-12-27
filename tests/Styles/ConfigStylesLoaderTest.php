a70c471f0f41aa110de9f04e2232c1047f69b534
AlexMenschoid <meyer-schlichtmann@demos-deutschland.de>
2021-12-27 11:11:35 +0100

    feat: adds ConfigStylesLoaderTest

---
+++ tests/Styles/ConfigStylesLoaderTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DemosEurope\DocumentBakery\Tests\Styles;
+
+use DemosEurope\DocumentBakery\Styles\ConfigStylesLoader;
+use PHPUnit\Framework\TestCase;
+
+class ConfigStylesLoaderTest extends TestCase
+{
+    private ConfigStylesLoader $sut;
+
+    private array $testData = [
+        'recipe1' => [],
+        'recipe2' => [
+            'format' => [],
+            'instructions' => [],
+            'queries' => []
+        ]
+    ];
+
+    protected function setUp(): void
+    {
+        $this->sut = new ConfigStylesLoader($this->testData);
+    }
+
+    public function testLoad(): void
+    {
+        self::assertEquals($this->testData['recipe2'], $this->sut->load('recipe2'));
+    }
+
+    public function testGetName(): void
+    {
+        self::assertEquals(ConfigStylesLoader::class, $this->sut::getName());
+    }
+
+    public function testAvailableRecipes(): void
+    {
+        self::assertEquals($this->testData, $this->sut->availableStyles());
+    }
+}
