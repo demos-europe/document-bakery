1d5cfa6a275e2aa57ccd9af1e4f3001b8ebb1fb1
AlexMenschoid <meyer-schlichtmann@demos-deutschland.de>
2021-12-30 14:13:34 +0100

    feat(refs #5): adds InstructionFactoryTest

---
+++ tests/Instructions/InstructionFactoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DemosEurope\DocumentBakery\Tests\Instructions;
+
+use DemosEurope\DocumentBakery\Exceptions\DocumentGenerationException;
+use DemosEurope\DocumentBakery\Instructions\InstructionFactory;
+use DemosEurope\DocumentBakery\Instructions\Text;
+use DemosEurope\DocumentBakery\Instructions\UnorderedListItem;
+use DemosEurope\DocumentBakery\TwigRenderer;
+use PHPUnit\Framework\TestCase;
+
+class InstructionFactoryTest extends TestCase
+{
+
+    public function testLookupForName()
+    {
+        $twigRenderer = new TwigRenderer();
+        $instructions = [
+            'Text' => new Text($twigRenderer),
+            'UnorderedListItem' => new UnorderedListItem($twigRenderer)
+        ];
+
+        $sut = new InstructionFactory(new \ArrayObject($instructions));
+
+        $testObject1 = $sut->lookupForName('Text');
+        self::assertEquals($instructions['Text'], $testObject1);
+
+        $this->expectException(DocumentGenerationException::class);
+        $testObject2 = $sut->lookupForName('WrongName');
+    }
+}
