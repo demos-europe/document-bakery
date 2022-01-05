2a9bc7e520cb89758d650a7c0eefcb5230d8c33b
Stefan Graupner <graupner@demos-deutschland.de>
2022-01-05 09:41:06 +0100

    chore: Reduce code duplication

--- tests/DocumentBakeryBundleTest.php
+++ tests/DocumentBakeryBundleTest.php
@@ -5,40 +5,16 @@ declare(strict_types=1);
 namespace DemosEurope\DocumentBakery\Tests;
 
 use DemosEurope\DocumentBakery\Bakery;
-use DemosEurope\DocumentBakery\DocumentBakeryBundle;
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Nyholm\BundleTest\AppKernel;
-use Nyholm\BundleTest\BaseBundleTestCase;
-use Symfony\Component\HttpKernel\KernelInterface;
 
-class DocumentBakeryBundleTest extends BaseBundleTestCase
+class DocumentBakeryBundleTest extends KernelTestCase
 {
-    protected function createKernel(): KernelInterface
-    {
-        /**
-         * @var AppKernel $kernel
-         */
-        $kernel = parent::createKernel();
-        $kernel->addBundle(DoctrineBundle::class);
-        $kernel->addConfigFile(__DIR__.'/resources/doctrine.yml');
-        $kernel->addConfigFile(__DIR__.'/resources/services.yml');
-
-        return $kernel;
-    }
-    
     public function testBundle(): void
     {
-        $kernel = $this->createKernel();
-        $kernel->boot();
+        $this->setUp();
 
-        $container = $kernel->getContainer();
+        $container = $this->getContainer();
 
-        $bakery = $container->get(Bakery::class );
+        $bakery = $container->get(Bakery::class);
         self::assertInstanceOf(Bakery::class, $bakery);
-    }
-
-    protected function getBundleClass(): string
-    {
-        return DocumentBakeryBundle::class;
     }
 }
