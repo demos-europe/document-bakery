e0b55a43cffb4bc7cfa2fa17622a8296c2e1164e
Stefan Graupner <graupner@demos-deutschland.de>
2022-01-19 17:02:10 +0100

    feat: Setup for tests needing a database

--- tests/KernelTestCase.php
+++ tests/KernelTestCase.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace DemosEurope\DocumentBakery\Tests;
 
 
+use DAMA\DoctrineTestBundle\DAMADoctrineTestBundle;
 use DemosEurope\DocumentBakery\DocumentBakeryBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Nyholm\BundleTest\AppKernel;
 use Nyholm\BundleTest\BaseBundleTestCase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
@@ -25,6 +25,8 @@ class KernelTestCase extends BaseBundleTestCase
         $kernel = parent::createKernel();
 
         $kernel->addBundle(DoctrineBundle::class);
+        $kernel->addBundle(DAMADoctrineTestBundle::class);
+
         $kernel->addConfigFile(__DIR__.'/resources/doctrine.yml');
         $kernel->addConfigFile(__DIR__.'/resources/services.yml');
 
